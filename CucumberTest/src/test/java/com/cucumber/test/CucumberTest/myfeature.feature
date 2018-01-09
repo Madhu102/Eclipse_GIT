17288acb1f0ce871f995862090bf62362540270b
nisum <mk.vuppugalla@gmail.com>
2018-01-09 16:45:18 +0530

    Commit1

---
+++ CucumberTest/src/test/java/com/cucumber/test/CucumberTest/myfeature.feature
@@ -0,0 +1,8 @@
+Feature: Test feature
+ Scenario: my scenario
+ Given I am in login page of facebook
+ When I enter the user name
+ And I enter the password
+ Then I click on Login button.
+ And i am in Facebook wall.
+ 
---
+++ CucumberTest/src/test/java/com/cucumber/test/CucumberTest/steps/StepDefi.java
@@ -0,0 +1,42 @@
+package com.cucumber.test.CucumberTest.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefi {
+		
+		@Given("^I am in login page of facebook$")
+		public void i_am_in_login_page_of_facebook() throws Throwable {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new PendingException();
+		}
+
+		@When("^I enter the user name$")
+		public void i_enter_the_user_name() throws Throwable {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new PendingException();
+		}
+
+		@When("^I enter the password$")
+		public void i_enter_the_password() throws Throwable {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new PendingException();
+		}
+
+		@Then("^I click on Login button\\.$")
+		public void i_click_on_Login_button() throws Throwable {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new PendingException();
+		}
+
+		@Then("^i am in Facebook wall\\.$")
+		public void i_am_in_Facebook_wall() throws Throwable {
+		    // Write code here that turns the phrase above into concrete actions
+		    throw new PendingException();
+		}
+
+	}
+
+
